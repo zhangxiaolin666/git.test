aff800de49341bcdecc134293127e7b055e867d8
zhangxiaolin666 <76454395@qq.com>
2022-02-07 21:30:27 +0800

    second commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -3,5 +3,7 @@ package com.atguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+        System.out.println("Hello Git1!");
+        System.out.println("Hello Git2!");
     }
 }
